0a5cf6c1976fbeba435e9ff9c48813e9efe0ab44
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-11-01 17:32:25 +0200

    added connection to backend

--- js/main.js
+++ js/main.js
@@ -7,54 +7,23 @@ const createGemstoneName = document.getElementById('create_name');
 const createGemstoneWeight = document.getElementById('create_weightInCarats');
 const createGemstonePrice = document.getElementById('create_priceInUSD');
 
-var editActive = false;
+let editActive = false;
 
-let gemstones = [
-    {
-        "id": 1,
-        "name": "Diamond",
-        "weightInCarats": 0.02,
-        "priceInUSD": 1000 
-    },
-    {
-        "id": 2,
-        "name": "Ruby",
-        "weightInCarats": 0.05,
-        "priceInUSD": 5000 
-    },
-    {
-        "id": 3,
-        "name": "Nephrite",
-        "weightInCarats": 0.07,
-        "priceInUSD": 700 
-    },
-    {
-        "id": 4,
-        "name": "Diamond",
-        "weightInCarats": 0.08,
-        "priceInUSD": 6000 
-    },
-    {
-        "id": 5,
-        "name": "Ruby",
-        "weightInCarats": 0.07,
-        "priceInUSD": 6500 
-    },
-    {
-        "id": 6,
-        "name": "Emerald",
-        "weightInCarats": 0.03,
-        "priceInUSD": 2000 
-    },
-    {
-        "id": 7,
-        "name": "Perl",
-        "weightInCarats": 0.1,
-        "priceInUSD": 1000 
+const gems_url = 'http://localhost:5000/gemstone';
+
+let gemstones = [];
+function fetchData(url){
+fetch(url)
+.then(response => response.json())
+.then(data => { 
+    for (i = 0; i < data.length; i++){
+        gemstones.push(data[i]);    
     }
-]
-let currentGemstones = gemstones;
+    displayGemstones(gemstones);
+  });
+}
 
+let currentGemstones = gemstones
 
 searchBar.addEventListener('keyup', filterGemstones)
 function filterGemstones(searchString){
@@ -62,7 +31,6 @@ function filterGemstones(searchString){
     const filteredGemstones = gemstones.filter(gemstone =>{
         return gemstone.name.toLowerCase().includes(searchFilterString);
     });
-    console.log(filteredGemstones);
     currentGemstones = filteredGemstones;
     visualiseSortedGemstones();
 }
@@ -76,7 +44,7 @@ clearButton.addEventListener('click', ()=> {
 function calculatePrice(){
     var priceSum = 0;
     var totalPriceLabel = document.getElementById('total-price');
-    currentGemstones.forEach(gemstone => priceSum += gemstone.priceInUSD);
+    currentGemstones.forEach(gemstone => priceSum += gemstone.price_in_usd_dollars);
     totalPriceLabel.textContent = 'Total price: ' + priceSum + '$';
 }
 
@@ -109,17 +77,11 @@ function compareByName(firstGemstone, secondGemstone){
     }
     return 0;
 }
-
 function compareByWeight(firstGemstone, secondGemstone){
-    return firstGemstone.weightInCarats - secondGemstone.weightInCarats;
+    return firstGemstone.weight_in_carats - secondGemstone.weight_in_carats;
 }
-
 function compareByPrice(firstGemstone, secondGemstone){
-    return firstGemstone.priceInUSD - secondGemstone.priceInUSD;
-}
-
-function deleteRecord(element){
-    console.log(element);
+    return firstGemstone.price_in_usd_dollars - secondGemstone.price_in_usd_dollars;
 }
 
 // function to show current gems
@@ -130,13 +92,13 @@ const displayGemstones = (gemstonesToShow) => {
             <div>            
                 <h2 class="gem_id">${gemstone.id}</h2>
                 <h2>${gemstone.name}</h2>
-                <h3 class="weightInCarats">${gemstone.weightInCarats}</h3>
-                <h3 class="priceInUSD">${gemstone.priceInUSD}</h3>
+                <h3 class="weightInCarats">${gemstone.weight_in_carats}</h3>
+                <h3 class="priceInUSD">${gemstone.price_in_usd_dollars}</h3>
             </div>
             <form class="form__edit_gemstone" id="form__edit_gemstone">
                     <input id="edit_name" name="name" type="text" placeholder="Name">
                     <input id="edit_weightInCarats" name="weightInCarats" type="number" step=0.1 placeholder="Weight">
-                    <input id="edit_priceInUSD" name="priceInUSD" type="number" placeholder="Price">
+                    <input id="edit_priceInUSD" name="priceInUSD" type="number" step=100 placeholder="Price">
             </form>
             <div class= "control-buttons">
                 <button class="edit-button" id="edit-button" onclick="editRecord(this)">Edit</button>
@@ -148,18 +110,18 @@ const displayGemstones = (gemstonesToShow) => {
 
     gemstoneList.innerHTML = htmlString;
 }
+
 function deleteRecord(record){
     const list_to_delete = record.parentNode.parentNode;
     let gemstoneId = parseInt(list_to_delete.childNodes[1].childNodes[1].innerHTML);
     let indexToDeleteFromAll = gemstones.findIndex(obj => obj.id==gemstoneId);
     gemstones.splice(indexToDeleteFromAll, 1);
-    if (searchBar.value != ''){
-        let indexToDeleteFromCurrent = currentGemstones.findIndex(obj => obj.id==gemstoneId);
-        console.log(indexToDeleteFromCurrent);
+    let indexToDeleteFromCurrent = currentGemstones.findIndex(obj => obj.id==gemstoneId);
+    if (indexToDeleteFromCurrent != -1){
         currentGemstones.splice(indexToDeleteFromCurrent, 1);
     }
+    deleteGemstone(gemstoneId);
     visualiseSortedGemstones();
-    console.log(gemstones, currentGemstones);
     return list_to_delete;
 }
 function editRecord(record){
@@ -176,70 +138,119 @@ function editRecord(record){
     
     let indexToEdit = gemstones.findIndex(obj => obj.id==gemstoneId);
     if (editActive == false){
-        editBar.classList.add('open');
-        editBar.classList.remove('hide');
-        infoBar.classList.add('hide');
-        infoBar.classList.remove('open');
-        editActive = true
+        openEditBar(editBar, infoBar);
+        editActive = true;
     } else if (editActive == true){
-        editBar.classList.add('hide');
-        editBar.classList.remove('open');
-        infoBar.classList.add('open');
-        infoBar.classList.remove('hide');
-
+        closeEditBar(editBar, infoBar);
         if (validateWeightAndPrice(editedGemstoneWeight.value, editedGemstonePrice.value) == false){
             editedGemstoneWeight.value = '';
             editedGemstonePrice.value = '';
+            editActive = false;
             return;
         }
-
+        let finalName = gemstoneName;
+        let finalWeight = gemstoneWeight;
+        let finalPrice = gemstonePrice;
+        if (editedGemstoneName.value == "" && editedGemstoneWeight.value == "" && editedGemstonePrice.value == ""){
+            editActive = false;
+            visualiseSortedGemstones();
+            return
+        }
         if (editedGemstoneName.value != "") {
             gemstones[indexToEdit]["name"] = editedGemstoneName.value;
+            finalName = editedGemstoneName.value;
         } else {
             gemstones[indexToEdit]["name"] = gemstoneName;
         }
         if (editedGemstoneWeight.value != "") {
-            gemstones[indexToEdit]["weightInCarats"] = parseFloat(editedGemstoneWeight.value);
+            gemstones[indexToEdit]["weight_in_carats"] = parseFloat(editedGemstoneWeight.value);
+            finalWeight =  parseFloat(editedGemstoneWeight.value);
         } else{
-            gemstones[indexToEdit]["weightInCarats"] = gemstoneWeight;
+            gemstones[indexToEdit]["weight_in_carats"] = gemstoneWeight;
         }
         if (editedGemstonePrice.value != "") {
-            gemstones[indexToEdit]["priceInUSD"] =  parseFloat(editedGemstonePrice.value)
+            gemstones[indexToEdit]["price_in_usd_dollars"] =  parseFloat(editedGemstonePrice.value);
+            finalPrice = parseFloat(editedGemstonePrice.value);
         } else{
-            gemstones[indexToEdit]["priceInUSD"] =  gemstonePrice
+            gemstones[indexToEdit]["price_in_usd_dollars"] = gemstonePrice;
         }
-        
+
+        if (searchBar.value != '' && editedGemstoneName.value != '' && editedGemstoneName.value.includes(searchBar.value) == false){
+            let indexToDeleteFromCurrent = currentGemstones.findIndex(obj => obj.id==gemstoneId);
+            currentGemstones.splice(indexToDeleteFromCurrent, 1);
+        }
+
+        const jsonGemstone = createJSON(finalName, finalWeight, finalPrice)
+        editGemstone(gemstoneId, jsonGemstone)
         editActive = false;
         visualiseSortedGemstones();
     }
 }
-function createGemstone(){
-    if(validateFormRequirements(createGemstoneID.value, createGemstoneName.value, createGemstoneWeight.value, createGemstonePrice.value) == false){
-        console.log('error');
+
+function openEditBar(editBar, infoBar){
+    editBar.classList.add('open');
+    editBar.classList.remove('hide');
+    infoBar.classList.add('hide');
+    infoBar.classList.remove('open');
+}
+
+function closeEditBar(editBar, infoBar){
+    editBar.classList.add('hide');
+    editBar.classList.remove('open');
+    infoBar.classList.add('open');
+    infoBar.classList.remove('hide');
+}
+async function createGemstone(){
+    if(validateFormRequirements(createGemstoneName.value, createGemstoneWeight.value, createGemstonePrice.value) == false){
         return;
     }
     if(validateWeightAndPrice(createGemstoneWeight.value, createGemstonePrice.value) == false){
-        console.log('error');
         return;
     }
-    let json = createJSON(createGemstoneID.value, createGemstoneName.value, createGemstoneWeight.value, createGemstonePrice.value);
-    
-    gemstones.push(json)
-
+    const jsonGemstone = createJSON(createGemstoneName.value, createGemstoneWeight.value, createGemstonePrice.value);
+    await postGemstone(jsonGemstone);
     visualiseSortedGemstones();
-    return json;
+    return jsonGemstone;
+}
+async function postGemstone(newGemstone) {    
+    let response = await fetch(gems_url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(newGemstone)
+  }).then(response => response.json())
+  .then(data => gemstones.push(data))
+  return response;
 }
 
-function createJSON(id, name, weight, price){
+async function deleteGemstone(id){
+    let response = await fetch(gems_url + '/' + id, {
+    method: 'DELETE',
+    })
+    return response;
+}
+async function editGemstone(id, editedGemstone){
+    fetch(gems_url + '/' + id, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+    body: JSON.stringify(editedGemstone)
+    })
+}
+function createJSON(name, weight, price){
     let createdGemstone = {
-        "id": parseInt(id),
         "name": name,
-        "weightInCarats": parseFloat(weight),
-        "priceInUSD": parseFloat(price)
+        "weight_in_carats": parseFloat(weight),
+        "price_in_usd_dollars": parseFloat(price)
     }
     return createdGemstone;
-
 }
+
+
+// functions for validating data
+
 function validateWeightAndPrice(weight, price){
     if (parseFloat(weight) <=0){
         alert('weight cannot be less then zero');
@@ -251,11 +262,7 @@ function validateWeightAndPrice(weight, price){
     }
     return true;
 }
-function validateFormRequirements(id, name, weight, price){
-    if(id == ''){
-        alert('id field is requiered')
-        return false;
-    }
+function validateFormRequirements(name, weight, price){
     if(name == ''){
         alert('name field is requiered')
         return false;
@@ -271,6 +278,5 @@ function validateFormRequirements(id, name, weight, price){
     return true;
 }
 
-
 // show all gems at the load
-displayGemstones(currentGemstones)
+fetchData(gems_url);
